d3e87f836efb52d58a3790b573e4fd04423cc82a
sagaz <santiagomachadoviloria@gmail.com>
2025-03-11 18:45:24 -0500

    Commit

---
+++ descargas/src/Colas.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Colas {
+    private int numElememtos;  // typo in variable name, it should be numElementos
+    private int inicio;
+    private int end;
+    private String[] cola;
+
+    // Constructor
+    public Colas(int tam) {
+        cola = new String[tam];
+        this.numElememtos = 0;
+        this.inicio = 0;
+        this.end = -1;
+    }
+
+    public boolean vacia() {
+        return numElememtos == 0;
+    }
+
+    public boolean insertar(String elemento) {
+        if (numElememtos < cola.length) {
+            end = (end + 1) % cola.length;
+            cola[end] = elemento;
+            numElememtos++;
+            return true;
+        }
+        return false;
+    }
+
+    public String remover() {
+        if (vacia()) {
+            return null;
+        }
+        String eliminado = cola[inicio];
+        cola[inicio] = null;
+        inicio = (inicio + 1) % cola.length;
+        numElememtos--;
+        return eliminado;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(cola);
+    }
+
+}
---
+++ marcadores/src/ListasSimples.java
@@ -0,0 +1,2 @@
+public class ListasSimples {
+}
